dc523efa4850c84ac5a7631385375400d8fe9795
Stefano Bocconi <stefano@waag.org>
2017-01-11 15:56:47 +0100

    Also copy fonts

--- puphpet/files/exec-once/80_install-histograph-viewer.sh
+++ puphpet/files/exec-once/80_install-histograph-viewer.sh
@@ -36,5 +36,5 @@ cd ${SRC_HOME}/${MY_MODULE}
 sudo su $MYUSER -c "${NPM_INSTALL}"
 
 sudo su $MYUSER -c "HISTOGRAPH_CONFIG=\"${SRC_HOME}/config.yaml\" npm run production"
-cp -r config.json index.html css dist js images /var/www/${MY_MODULE}
+cp -r config.json index.html css dist js images fonts /var/www/${MY_MODULE}
 chown -R www-data:www-data /var/www/${MY_MODULE}
