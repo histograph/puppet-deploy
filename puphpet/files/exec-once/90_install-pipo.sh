972391af12e24a3e1597c61ce7d235d70ce42b02
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-01-31 20:48:00 +0100

    Add conf for pipo

--- puphpet/files/exec-once/90_install-pipo.sh
+++ puphpet/files/exec-once/90_install-pipo.sh
@@ -32,9 +32,68 @@ find ${MY_WEBDIR} -type f -exec rm -rf {} \;
 
 cp -r app images web vendor src ${MY_WEBDIR}
 
+cat > ${MY_WEBDIR}/app/config/parameters.php<<EOF
+<?php
+
+// SITE configs
+\$app['sitename'] = 'Histograph - Pit Importer Part One';
+\$app['upload_dir'] = __DIR__ . '/../storage/uploads';
+\$app['export_dir'] = __DIR__ . '/../storage/exports';
+\$app['api_user'] = '$(read_params histograph.users.api_user)';
+\$app['api_pass'] = '$(read_params histograph.users.api_password)';
+
+\$app['aws_key'] = '$(read_params histograph.aws.key)';
+\$app['aws_secret'] = '$(read_params histograph.aws.secret)';
+\$app['aws_bucket'] = '$(read_params histograph.aws.bucket)';
+\$app['aws_region'] = '$(read_params histograph.aws.region)';
+
+// BASIC USERS with PASSWORD
+\$app['users'] = array(
+    '$(read_params histograph.users.import_user)' => '$(read_params histograph.users.import_password)',
+);
+
+// FOR DEVELOPMENT
+//\$app['debug'] = true;
+
+// FOR PRODUCTION
+\$app['debug'] = false;
+
+
+// LOCALE
+\$app['locale'] = 'nl';
+\$app['session.default_locale'] = \$app['locale'];
+
+
+// DOCTRINE DBAL
+\$app["db.options"] = array(
+    'driver'   => 'pdo_mysql',
+    'host'     => 'localhost',
+    'user'     => '$(read_params mysql.users.histograph.name)',
+    'password' => '$(read_params mysql.users.histograph.password)',
+    'dbname'   => '$(read_params mysql.databases.histograph.name)',
+    'charset'   => 'utf8',
+);
+
+// MONOLOG
+\$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../../app/storage/log/prod.log',
+    'monolog.name'    => 'Pipo-app',
+    'monolog.level'   => Monolog\Logger::WARNING,
+    //'monolog.level'   => Monolog\Logger::DEBUG,
+));
+
+// CACHES
+\$app['cache.path'] = __DIR__ . '/../storage/cache';
+// Http cache
+\$app['http_cache.cache_dir'] = \$app['cache.path'] . '/http';
+// Twig cache
+\$app['twig.options.cache'] = \$app['cache.path'] . '/twig';
+EOF
+
 chown -R www-data:www-data ${MY_WEBDIR}
 
 # chmod -R ug+wxrX ${MY_WEBDIR}/app/storage/cache ${MY_WEBDIR}/app/storage/log ${MY_WEBDIR}/app/storage/uploads
 chmod -R ug+wxrX ${MY_WEBDIR}
+chmod -R o-wxrX ${MY_WEBDIR}
 
 service nginx restart
