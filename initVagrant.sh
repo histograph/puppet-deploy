ba0ba7a20d9b52092d076faa35ff08a9b41c98d7
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-02-28 22:44:57 +0100

    Adding first support for AWS deploy

--- initVagrant.sh
+++ initVagrant.sh
@@ -1,24 +1,80 @@
 #!/usr/bin/env bash
 
+if [ "$(echo $0)" != "-bash" ]
+then
+	echo "Run the command with source!!!"
+	exit 1
+fi
+
+MY_CMD=$1
+
+if [ -z "${MY_CMD}" ]
+then
+	echo "You must specify either LOCAL, AWS or HETZNER, exiting"
+	return 1
+fi
+
 V_VERS="1.8.7"
-# PLUGIN_LIST="landrush vagrant-aws vagrant-managed-servers vagrant-share vagrant-triggers vagrant-bindfs"
-PLUGIN_LIST="landrush vagrant-bindfs"
 
 if ! vagrant --version | grep "${V_VERS}" >/dev/null
 then
   echo "WARNING: environment tested with ${V_VERS}, you have $(vagrant --version)"
 fi
 
-MY_BOX=$(grep 'box: ' puphpet/config.yaml | sed 's/.*box: \(.*\)/\1/g')
-MY_BOXVERSION=$(grep 'box_version: ' puphpet/config.yaml | sed "s/.*box_version: '\(.*\)'/\1/g")
-
-if ! vagrant box list | grep ${MY_BOX} | grep ${MY_BOXVERSION} > /dev/null
+if [ "${MY_CMD}" = "AWS" ]
 then
-  echo "Adding box ${MY_BOX} version ${MY_BOXVERSION}"
-  vagrant box add --provider virtualbox --box-version ${MY_BOXVERSION} ${MY_BOX}
+  echo "Setting Vagrant for AWS"
+  if ! vagrant plugin list | grep vagrant-aws
+  then
+    vagrant plugin install vagrant-aws
+  fi
+
+  if ! vagrant box list | grep dummy | grep aws
+  then
+    vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+  fi
+
+  export VAGRANT_DEFAULT_PROVIDER=aws && echo VAGRANT_DEFAULT_PROVIDER=$VAGRANT_DEFAULT_PROVIDER!!
+  export AWS_ACCESS_KEY_ID=$(cat ~/.aws/credentials | grep -i AWS_ACCESS_KEY_ID | tr -d ' ' | cut -d'=' -f2) && echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID!!
+  export AWS_SECRET_ACCESS_KEY=$(cat ~/.aws/credentials | grep -i AWS_SECRET_ACCESS_KEY | tr -d ' ' | cut -d'=' -f2) && echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY!!
+elif [ "${MY_CMD}" = "LOCAL" ]
+then
+  echo "Resetting Vagrant to local environment and preparing repositories"
+  unset VAGRANT_DEFAULT_PROVIDER
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+
+  export VAGRANT_DEFAULT_PROVIDER=local && echo VAGRANT_DEFAULT_PROVIDER=$VAGRANT_DEFAULT_PROVIDER!!
+
+  MY_BOX=$(grep 'box: ' puphpet/config.yaml | sed 's/.*box: \(.*\)/\1/g')
+  MY_BOXVERSION=$(grep 'box_version: ' puphpet/config.yaml | sed "s/.*box_version: '\(.*\)'/\1/g")
+
+  if ! vagrant box list | grep ${MY_BOX} | grep ${MY_BOXVERSION} > /dev/null
+  then
+    echo "Adding box ${MY_BOX} version ${MY_BOXVERSION}"
+    vagrant box add --provider virtualbox --box-version ${MY_BOXVERSION} ${MY_BOX}
+  fi
+
+  if [ ! -f ./puphpet/files/dot/ssh/id_rsa ]
+  then
+    echo "Key not installed, get it from the conf dir on OwnCloud"
+    exit 1
+  fi
+
+	if [ ! -d ../histograph ]
+	then
+    echo "Creating histograph directory for local development"
+		mkdir -P ../histograph
+	fi
+elif [ "${MY_CMD}" = "HETZNER" ]
+then
+  echo "Hetzer is not implemented yet"
+  exit 1
 fi
 
 PLUGINS="$(vagrant plugin list)"
+# PLUGIN_LIST="landrush vagrant-aws vagrant-managed-servers vagrant-share vagrant-triggers vagrant-bindfs"
+PLUGIN_LIST="landrush vagrant-bindfs"
 
 
 for i in ${PLUGIN_LIST}
@@ -28,12 +84,6 @@ do
     vagrant plugin install "${i}"
   fi
 done
-
-if [ ! -f ./puphpet/files/dot/ssh/id_rsa ]
-then
-  echo "Key not installed, get it from the conf dir on OwnCloud"
-  exit 1
-fi
 
 # for i in $(puppet module --modulepath ./modules list | sed -E 's/├──( [a-zA-Z0-9\-]+) \(.*\)/\1/g' | grep "^ " | tr '\n' ' '); do puppet module --module-path ./modules upgrade ${i}; done
 
