86f72a72d17b9e6326a66bb8660a9725cf6b35de
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-03-09 16:05:49 +0100

    correct parameter (was not checked in by the previous check-in)

--- puphpet/files/exec-once/60_install-histograph-data.sh
+++ puphpet/files/exec-once/60_install-histograph-data.sh
@@ -12,10 +12,10 @@ source $(dirname $0)/set-vars "${1}"
 # install histograph-data
 install_data
 
-if [ "$(read_params  histograph.repositories.data.doDownload) " == "true " ]
+if [ "$(read_params  histograph.data.doDownload) " == "true " ]
 then
   echo "Download repos ${DATA_REP_TODOWNLOAD}"
-  
+
   cd ${SRC_HOME}/data
   sudo su $MYUSER -c "node index.js --config ${SRC_HOME}/config.yaml ${DATA_REP_TODOWNLOAD}"
 fi
