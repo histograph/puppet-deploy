0118af7a92179011ab7c342b1e795a940766061f
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-03-13 12:00:49 +0100

    Add log rotate for log files

---
+++ puphpet/files/exec-once/500_set-logrotate.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# run as root
+echo
+echo "  ---%%%%%%%%%%%%%%%%%%%%%%%%%%---"
+echo "----%%%%% INSTALLING E&L GEOTHESAURUS %%%%%----"
+echo "  ---%%%%%%%%%%%%%%%%%%%%%%%%%%---"
+echo
+
+#set -x
+
+LOGROTATE_HOME="/etc/logrotate.d"
+
+
+cat > ${LOGROTATE_HOME}/elasticsearch << ELASTICSEARCH
+/var/log/elasticsearch/${ES_INSTNAME}/*.log {
+  copytruncate
+  daily
+  rotate 7
+  compress
+  missingok
+  notifempty
+  size 10M
+}
+ELASTICSEARCH
+
+
+cat > ${LOGROTATE_HOME}/histograph << HISTOGRAPH
+/var/log/histograph/*.log {
+  copytruncate
+  daily
+  rotate 7
+  compress
+  missingok
+  notifempty
+  size 50M
+}
+HISTOGRAPH
+
+cat > ${LOGROTATE_HOME}/neo4j << NEO4J
+/var/log/neo4j/*.log {
+	su neo4j neo4j
+	copytruncate
+	rotate 7
+	daily
+	compress
+	missingok
+	notifempty
+  size 10M
+}
+NEO4J
