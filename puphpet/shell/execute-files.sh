e48c469187cef0044a272242a0c8981ce0f910d6
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-10-30 16:14:15 +0100

    run erfgeo installer every time

--- puphpet/shell/execute-files.sh
+++ puphpet/shell/execute-files.sh
@@ -43,7 +43,7 @@ echo "Running files in files/${EXEC_ALWAYS_DIR}"
 
 find "${VAGRANT_CORE_FOLDER}/files/${EXEC_ALWAYS_DIR}" -maxdepth 1 -type f -name '*.sh' | sort | while read FILENAME; do
     chmod +x "${FILENAME}"
-    /bin/bash "${FILENAME}"
+    /bin/bash "${FILENAME}" "${HOSTNAME_VM}"
 done
 
 echo "Finished running files in files/${EXEC_ALWAYS_DIR}"
